bcb2de5403552ee4978dc2fefe8fad54694cecd7
Nami-R2301 <je191030@ens.uqam.ca>
2020-11-17 18:47:02 -0500

    Added malib.h/c

--- tp2.c
+++ tp2.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#include <time.h>
-#include <math.h>
 #include <string.h>
 #include "tcv.h"
+#include "mabib.h"
 
 int main(void) {
 
-  version_t *v = {};
-  getVersion(v);
   char ligne[30];
 
   printf("\n\n\nHello World\n\n\n");

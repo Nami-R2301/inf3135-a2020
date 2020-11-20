f501f468bb9c7aa98612a6df7a64d62226decf06
Nami-R2301 <je191030@ens.uqam.ca>
2020-11-19 23:31:16 -0500

    Boolean logic applied.

--- tp2.c
+++ tp2.c
@@ -2,36 +2,28 @@
 
 int main(void) {
 
+  bool valideId = false;
+  int ligne = 0;
+//  bool valideTempH = false;
+//  bool valideTempA = false;
+//  bool validePpm = false;
   version_t v = {0, 1, 1000};
   getVersion(&v);
   printf("Version #: %hhu.%hhu.%u\n", v.major, v.minor, v.build);
-  struct identification_s id = {0, "00", 9999, 2};
-  int ligne = scanf("%zu %s %zu %hhu", &id.timestamp, id.event, &id.id, &id.puissanceEmetteur);
+  do {
 
-  if (ligne == 4 && !feof(stdin)) {
+    struct identification_s id = {0, "00", 9999, 2};
+//    struct tempHumaine_s tempH = {id.timestamp, "01", 36.9};
 
-    evenementId(id.timestamp, id.event, id.id, id.puissanceEmetteur);
-    printf("%s\n", id.event);
-  }
-
-/**  do {
-
-    struct tempHumaine_s tempH = {id.timestamp, "01", 36.9};
-
-    bool valideTempH = false;
-    bool valideTempA = false;
-    bool validePpm = false;
-    size_t tempTime = 0;
-    char tempEvent[3];
-    float tempDegrees = 0.0;
-    ligne = scanf( "%zu %s %f", &tempTime, tempEvent, &tempDegrees);
-
-    if(ligne == 3 && !valideTempH && strcmp(tempEvent, "01") == 0) {
-      valideId = evenementTempH();
+    if(!valideId) {
+      ligne = scanf("%zu %s %zu %hhu", &id.timestamp, id.event, &id.id, &id.puissanceEmetteur);
+      valideId = evenementId(id.timestamp, id.event, id.id, id.puissanceEmetteur);
     }
 
-  } while(!valideTempH && !valideTempA && !validePpm && !feof(stdin));
-*/
+//    if(valideId && !valideTempH) {
+//      valideTempH = evenementTempH(&tempH.id.timestamp, tempH.event, tempH.degrees);
+//    }
 
+  } while(!valideId && ligne != 0);
   return 0;
 }

140af532345968598284fa85875290f9baecdbb4
Nami-R2301 <je191030@ens.uqam.ca>
2020-11-20 23:13:27 -0500

    Fixed Do/While + strtoul & atoi.

--- tp2.c
+++ tp2.c
@@ -2,9 +2,9 @@
 
 int main(void) {
 
-  bool valideId = false;
   int ligne = 0;
-  bool valideTempH = false;
+  bool valideId = false;
+//  bool valideTempH = false;
 //  bool valideTempA = false;
 //  bool validePpm = false;
   version_t v = {0, 1, 1000};
@@ -12,21 +12,34 @@ int main(void) {
   printf("Version #: %hhu.%hhu.%u\n", v.major, v.minor, v.build);
   do {
 
-    struct invalideSim = {0, 0, 0};
-    struct erreur_s erreurSim = {0, 0, 0};
-    struct identification_s id = {0, "00", 9999, 2};
-    struct tempHumaine_s tempH = {id.timestamp, "01", 36.9};
+    struct temp_s prev = {0, "00", "", ""};
+    struct temp_s courant = {0, "No", "ID", "Power"};
+//    struct tempHumaine_s tempH = {id.timestamp, "01", 0.0};
+//    struct tempAmbiante_s tempA = {tempH.timestamp, "02", 20.0};
 
-    if(!valideId) {
-      ligne = scanf("%zu %s %zu %hhu", &id.timestamp, id.event, &id.id, &id.puissanceEmetteur);
-      valideId = sortieDix(10, id.timestamp, id.id, id.puissanceEmetteur);
+    ligne = scanf("%zu %s %s %s", &courant.argUn, courant.argDeux, courant.argTrois, courant.argQuatre);
+
+    if(!valideId && ligne >= 2 && strcmp(courant.argDeux, "00") == 0 && prev.argUn >= courant.argUn) {
+
+      valideId = sortieDix(10, courant.argUn, strtoul(courant.argTrois, NULL, 0), (unsigned) atoi(courant.argQuatre));
     }
 
-    if(valideId && !valideTempH) {
-      ligne = scanf("%zu %s %.1f", &tempH.timestamp, tempH.event, &tempH.degrees);
-      valideTempH = evenementTempH(tempH.timestamp, tempH.event, tempH.degrees);
-    }
+//    if(prev.argUn >= ) {
 
-  } while(!valideId && !valideTempH);
+//      ligne = scanf("%zu %s %.1f", &tempH.timestamp, tempH.event, &tempH.degrees);
+//      valideTempH = evenementTempH(tempH.timestamp, tempH.event, tempH.degrees);
+//    }
+
+//    if(valideId && valideTempH && !valideTempA) {
+
+//      ligne = scanf("%zu %s %.1f", &tempA.timestamp, tempA.event, &tempA.degrees);
+//      valideTempA = evenementTempA(tempA.timestamp, tempA.event, tempA.degrees);
+//    }
+
+//    if(
+
+    prev = courant;
+
+  } while(!valideId && ligne != 0);
   return 0;
 }

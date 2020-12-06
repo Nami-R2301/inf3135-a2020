f6af1e23b6c665c81194441efe0598779eeb93d6
Nami-R2301 <je191030@ens.uqam.ca>
2020-12-06 13:55:07 -0500

    Refactor.

--- tp3.c
+++ tp3.c
@@ -9,109 +9,37 @@ int main() {
   ppm_t *ppm = (ppm_t*) malloc(sizeof(ppm_t));
   signal_t *signal = (signal_t*) malloc(sizeof(signal_t));
   echange_t *echange = (echange_t*) malloc(sizeof(echange_t));
-  version_t v;
-  getVersion(&v);
-  temp_t courant;
+  temp_t courant = initCourant();
+  unsigned int build = initVersion();
   char *input =  malloc(sizeof(courant));
 
-  printf("Version #: %hhu.%hhu.%u\n", v.major, v.minor, v.build);
+  while(ligne != -1 && fgets(input, sizeof(temp_t), stdin) != NULL) {
 
-    while(ligne != -1 && fgets(input, sizeof(temp_t), stdin) != NULL) {
+    ligne = sscanf(input, "%zu %s %s %s", &courant.timestamp, courant.event, courant.argTrois, courant.argQuatre); //pour qu'on puissent recevoir moins d'entrees (numLigne).
 
-      ligne = sscanf(input, "%zu %s %s %s", &courant.timestamp, courant.event, courant.argTrois, courant.argQuatre); //pour qu'on puissent recevoir moins d'entrees (numLigne).
-
-      if(ligne == 4 && courant.timestamp > mainId->timestamp && strcmp(courant.event, "00") == 0) {
-
-        sortieDix(10, mainId, courant);
-      }
-
-      if(ligne == 3 && courant.timestamp > mainId->timestamp && strcmp(courant.event, "01") == 0) {
-
-        if(strcmp(courant.argTrois, ERREUR) != 0) {
-
-          tempH->timestamp = courant.timestamp;
-          tempH->degrees += (float) atof(courant.argTrois);
-
-          if(tempHumaine(tempH->timestamp, tempH->degrees, v.build)) {
-            tempH->compteur++;
-          } else {
-              tempH->compteurInvalide++;
-            }
-        } else {
-            tempH->cumul++;
-
-            if (tempH->cumul % 3 == 0) {
-              tempH->compteurError++;
-            }
-          }
-      }
-
-      if(ligne == 3 && courant.timestamp > tempH->timestamp && strcmp(courant.event, "02") == 0) {
-
-        if(strcmp(courant.argTrois, ERREUR) != 0) {
-
-          tempA->timestamp = courant.timestamp;
-          tempA->degrees += (float) atof(courant.argTrois);
-
-          if(tempAmbiante(tempA->timestamp, tempA->degrees, v.build)) {
-            tempA->compteur++;
-          } else {
-              tempA->compteurInvalide++;
-            }
-        } else {
-            tempA->cumul++;
-
-            if (tempA->cumul % 3 == 0) {
-              tempA->compteurError++;
-            }
-          }
-      }
-
-      if(ligne == 3 && courant.timestamp > tempA->timestamp && strcmp(courant.event, "03") == 0) {
-
-        if(strcmp(courant.argTrois, ERREUR) != 0) {
-
-          ppm->timestamp = courant.timestamp;
-          ppm->ppm += atof(courant.argTrois);
-
-          if(pulsationMin(ppm->timestamp, ppm->ppm, v.build)) {
-            ppm->compteur++;
-          } else {
-              ppm->compteurInvalide++;
-            }
-        } else {
-            ppm->cumul++;
-
-            if (ppm->cumul % 3 == 0) {
-              ppm->compteurError++;
-            }
-          }
-      }
-
-      if(ligne == 4 && courant.timestamp > ppm->timestamp && strcmp(courant.event, "04") == 0) {
-
-        signal->timestamp = courant.timestamp;
-        signal->power = (signed short) atoi(courant.argTrois);
-        signal->id[signal->compteurId] = (size_t) strtoul(courant.argQuatre, NULL, 0);
-
-        if(signalRssi(signal->timestamp, signal->power, signal->id[signal->compteurId], v.build)) {
-          signal->compteurId++;
-        }
-        sortieQuatorze(14, signal->timestamp, signal->id[signal->compteurId], signal->power, *mainId);
-        signal->compteurId++;
-      }
-
-      if(ligne == 4 && courant.timestamp > signal->timestamp && strcmp(courant.event, "05") == 0) {
-
-        echange->timestamp = courant.timestamp;
-        echange->id = (size_t) strtoul(courant.argTrois, NULL, 0);
-        echange->idPn = (size_t) strtoul(courant.argQuatre, NULL, 0);
-//      echangeData(echange.timestamp, echange.id, *echange.idpn);
-      }
-
+    if(ligne == 4 && courant.timestamp >= mainId->timestamp && strcmp(courant.event, "00") == 0) {
+      sortieDix(mainId, courant);
     }
-      sortieVingtUn(21, *tempH, *tempA, *ppm);
-      sortieVingtDeux(22, tempH->compteurInvalide, tempA->compteurInvalide, ppm->compteurInvalide);
-      sortieVingtTrois(23, tempH->compteurError, tempA->compteurError, ppm->compteurError);
-      return 0;
+    if(ligne == 3 && courant.timestamp >= mainId->timestamp && strcmp(courant.event, "01") == 0) {
+      tempHumaine(tempH, courant, build);
+    }
+    if(ligne == 3 && courant.timestamp >= tempH->timestamp && strcmp(courant.event, "02") == 0) {
+      tempAmbiante(tempA, courant, build);
+    }
+    if(ligne == 3 && courant.timestamp >= tempA->timestamp && strcmp(courant.event, "03") == 0) {
+      pulsationMin(ppm, courant, build);
+    }
+    if(ligne == 4 && courant.timestamp >= ppm->timestamp && strcmp(courant.event, "04") == 0) {
+      sortieQuatorze(signal, courant, mainId, build);
+    }
+    if(ligne == 4 && courant.timestamp >= signal->timestamp && strcmp(courant.event, "05") == 0) {
+//      sortieQuinze(echange, courant, mainId, signal->id[0], build);
+    }
+  }
+    sortieVingtUn(*tempH, *tempA, *ppm);
+    sortieVingtDeux(tempH->compteurInvalide, tempA->compteurInvalide, ppm->compteurInvalide);
+    sortieVingtTrois(tempH->compteurError, tempA->compteurError, ppm->compteurError);
+    free(input);
+    vider(mainId, tempH, tempA, ppm, signal, echange);
+    return 0;
 }

3cd01b6fe64ab4cdabe8e1fe1bea80661af7625d
Nami-R2301 <je191030@ens.uqam.ca>
2020-12-15 02:53:54 -0500

    Input size fixed.

--- tp3.c
+++ tp3.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
   long prevTime = -1;
   printf("Version #: %hhu.%hhu.%u\n", courant->v->major, courant->v->minor, courant->v->build);
 
-  while(fgets(input, sizeof(temp_t) * sizeof(size_t), stdin) != NULL && strcmp(input, "\n") != 0) {
+  while(fgets(input, sizeof(temp_t), stdin) != NULL && strcmp(input, "\n") != 0) {
 
     ligne = sscanf(input, "%zu %s %s %s", &courant->timestamp, courant->event, courant->argTrois, courant->argQuatre);
 

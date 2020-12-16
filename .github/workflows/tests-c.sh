0b24bf28de71e68d3e65fb12c0a36e49bcc766b0
Nami-R2301 <59898504+Nami-R2301@users.noreply.github.com>
2020-12-16 17:26:18 -0500

    Update tests-c.sh

--- .github/workflows/tests-c.sh
+++ .github/workflows/tests-c.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo -e \!\[test-c\]\(https\:\/\/github.com\/Nami-R2301\/inf3135-a2020\/workflows\/test-c\/badge\.svg\?branch\=tp3\)\\n > compilation.md
+echo -e \#\# Tests de compilation pour le TP3 à jour:\\n >> compilation.md
 echo -e \`\`\`\\n >> compilation.md
 TZ=America/New_York date >> compilation.md
 echo -e \\n >> compilation.md

4766ebfc15d11785138acd7172554972b94fbe3c
Nami-R2301 <je191030@ens.uqam.ca>
2020-12-16 16:32:06 -0500

    Organized workflows

---
+++ .github/workflows/tests-c.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo -e \!\[test-c\]\(https\:\/\/github.com\/Nami-R2301\/inf3135-a2020\/workflows\/test-c\/badge\.svg\?branch\=tp3\)\\n > ../../compilation.md
+echo -e \`\`\`\\n >> ../../compilation.md
+TZ=America/New_York date >> ../../compilation.md
+echo -e \\n >> ../../compilation.md
+make >> ../../compilation.md
+echo -e \\n\`\`\` >> ../../compilation.md

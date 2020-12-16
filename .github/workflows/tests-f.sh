b6a5c7d0ce7d7466f3047894a516a96b67540602
Nami-R2301 <je191030@ens.uqam.ca>
2020-12-16 16:43:03 -0500

    Directory path fix

--- .github/workflows/tests-f.sh
+++ .github/workflows/tests-f.sh
@@ -2,20 +2,20 @@
 
 echo -e \\n\*\*\*\*\*\*Tests sans arguments\*\*\*\*\*\*\\n >> ../../fonctionnel.md
 echo -e \$ Fichier-tests-f\.txt \| \.\/tp3\\n >> ../../fonctionnel.md
-cat Fichier-tests-f.txt | ../../tp3 >> ../../fonctionnel.md
+cat .github/workflows/Fichier-tests-f.txt | bash tp3 >> ../../fonctionnel.md
 echo -e \\n\*\*\*\*\*\*Tests avec arguments\*\*\*\*\*\*\\n >> ../../fonctionnel.md
 echo -e \$ Fichier-tests-f\.txt \| \.\/tp3 -i -s -d\\n >> ../../fonctionnel.md
-cat Fichier-tests-f.txt | ../../tp3 -i -s -d >> ../../fonctionnel.md
+cat .github/workflows/Fichier-tests-f.txt | bash tp3 -i -s -d >> ../../fonctionnel.md
 echo -e \\n\$ Fichier-tests-f\.txt \| \.\/tp3 -t -s -d -i\\n >> ../../fonctionnel.md
-cat Fichier-tests-f.txt | ./tp3 -t -s -d -i >> ../../fonctionnel.md
+cat .github/workflows/Fichier-tests-f.txt | bash tp3 -t -s -d -i >> ../../fonctionnel.md
 echo -e \\n\$ Fichier-tests-f\.txt \| \.\/tp3 --t --s --b --d\\n >> ../../fonctionnel.md
-cat Fichier-tests-f.txt | ../../tp3 --t --s --b --d >> ../../fonctionnel.md
+cat .github/workflows/Fichier-tests-f.txt | bash tp3 --t --s --b --d >> ../../fonctionnel.md
 echo -e \\n\$ Fichier-tests-f\.txt \| \.\/tp3 -t\\n >> ../../fonctionnel.md
-cat Fichier-tests-f.txt | ../../tp3 -t >> ../../fonctionnel.md
+cat .github/workflows/Fichier-tests-f.txt | bash tp3 -t >> ../../fonctionnel.md
 echo -e \\n\*\*\*\*\*\*Tests avec STDIN vide\*\*\*\*\*\*\\n >> ../../fonctionnel.md
 echo -e \$ \.\/tp3 -t\\n >> ../../fonctionnel.md
-../../tp3 -t >> ../../fonctionnel.md
+bash tp3 -t >> ../../fonctionnel.md
 echo -e \\n\$ \.\/tp3 -i -s -d\\n >> ../../fonctionnel.md
-../../tp3 -i -s -d >> ../../fonctionnel.md
+bash tp3 -i -s -d >> ../../fonctionnel.md
 echo -e \\n\$ \.\/tp3\\n >> ../../fonctionnel.md
-../../tp3 >> ../../fonctionnel.md
+bash tp3 >> ../../fonctionnel.md

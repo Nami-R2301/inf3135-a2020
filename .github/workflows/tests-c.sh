29aedc4d62c88a0dd8eb0a6c3c0f39f64087634b
Nami-R2301 <59898504+Nami-R2301@users.noreply.github.com>
2020-12-17 01:41:01 -0500

    bracket missing fixed

--- .github/workflows/tests-c.sh
+++ .github/workflows/tests-c.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e \!\[test-c\]\(https\:\/\/github.com\/Nami-R2301\/inf3135-a2020\/workflows\/test-c\/badge\.svg\?branch\=tp3\)\\n > compilation.md
-echo -e \#\# Tests de compilation pour le TP3 à jour \($(TZ=America/New_York date) :\\n >> compilation.md
+echo -e \#\# Tests de compilation pour le TP3 à jour \($(TZ=America/New_York date\) :\\n >> compilation.md
 echo -e \`\`\`\\n >> compilation.md
 TZ=America/New_York date >> compilation.md
 echo -e \\n >> compilation.md

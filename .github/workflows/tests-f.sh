0cb16a91a9f223dca31aac0535a07734dd195319
Nami-R2301 <59898504+Nami-R2301@users.noreply.github.com>
2020-12-16 23:53:57 -0500

    Update tests-f.sh

--- .github/workflows/tests-f.sh
+++ .github/workflows/tests-f.sh
@@ -24,7 +24,9 @@ echo -e \\n\$ \.\/tp3 -i -s -d\\n >> fonctionnel.md
 echo -e \\n\$ \.\/tp3\\n >> fonctionnel.md
 ./tp3 >> fonctionnel.md
 echo -e \\n\*\*\*\*\*\*Tests avec valgrind\*\*\*\*\*\*\\n >> fonctionnel.md
-cat .github/workflows/Fichier-tests-f.txt | valgrind --log-file="valgrind.txt" valgrind.txt >> fonctionnel.md ./tp3 -i -s -d
+cat .github/workflows/Fichier-tests-f.txt | valgrind --log-file="valgrind.txt" ./tp3 -i -s -d
+cat valgrind.txt >> fonctionnel.md
 echo -e \\n >> fonctionnel.md
-valgrind --log-file="valgrind.txt" valgrind.txt >> fonctionnel.md ./tp3 -t -s -i -d
+valgrind --log-file="valgrind.txt" ./tp3 -t -s -i -d
+cat valgrind.txt >> fonctionnel.md
 echo -e \\n\`\`\` >> fonctionnel.md

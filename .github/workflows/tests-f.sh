7a22ccef521812ac621b3736ea867bbc3677a508
Nami-R2301 <59898504+Nami-R2301@users.noreply.github.com>
2020-12-17 01:15:05 -0500

    Added headers

--- .github/workflows/tests-f.sh
+++ .github/workflows/tests-f.sh
@@ -2,14 +2,18 @@
 
 echo -e \!\[test-f\]\(https\:\/\/github.com\/Nami-R2301\/inf3135-a2020\/workflows\/test-f\/badge\.svg\?branch\=tp3\)\\n > fonctionnel.md
 echo -e \#\# Tests fonctionnels pour le TP3 à jour: \\n >> fonctionnel.md
+echo -e \\n\\n\#\#\# Contenu de\/des Fichier\(s\) testé\(s\) :\\n >> fonctionnel.md
 echo -e \`\`\`\\n >> fonctionnel.md
 TZ=America/New_York date >> fonctionnel.md
-echo -e \\n\#\#\# Fichier\(s\) testé\(s\) :\\n >> fonctionnel.md
 cat .github/workflows/Fichier-tests-f.txt >> fonctionnel.md 
-echo -e \\n\*\*\*\*\*\*Tests sans arguments\*\*\*\*\*\*\\n >> fonctionnel.md
+echo -e \\n\`\`\`\\n >> fonctionnel.md
+echo -e \\n\#\#\# Tests sans arguments :\\n >> fonctionnel.md
+echo -e \`\`\`\\n >> fonctionnel.md
 echo -e \$ cat \.github\/workflows\/Fichier-tests-f\.txt \| \.\/tp3\\n >> fonctionnel.md
 cat .github/workflows/Fichier-tests-f.txt | ./tp3 >> fonctionnel.md
-echo -e \\n\*\*\*\*\*\*Tests avec arguments\*\*\*\*\*\*\\n >> fonctionnel.md
+echo -e \\n\`\`\`\\n >> fonctionnel.md
+echo -e \\n\#\#\# Tests avec arguments :\\n >> fonctionnel.md
+echo -e \\n\`\`\`\\n >> fonctionnel.md
 echo -e \$ cat \.github\/workflows\/Fichier-tests-f\.txt \| \.\/tp3 -i -s -d\\n >> fonctionnel.md
 cat .github/workflows/Fichier-tests-f.txt | ./tp3 -i -s -d >> fonctionnel.md
 echo -e \\n\$ cat \.github\/workflows\/Fichier-tests-f\.txt \| \.\/tp3 -t -s -d -i\\n >> fonctionnel.md
@@ -18,14 +22,18 @@ echo -e \\n\$ cat \.github\/workflows\/Fichier-tests-f\.txt \| \.\/tp3 --t --s -
 cat .github/workflows/Fichier-tests-f.txt | ./tp3 --t --s --b --d >> fonctionnel.md
 echo -e \\n\$ cat \.github\/workflows\/Fichier-tests-f\.txt \| \.\/tp3 -t\\n >> fonctionnel.md
 cat .github/workflows/Fichier-tests-f.txt | ./tp3 -t >> fonctionnel.md
-echo -e \\n\*\*\*\*\*\*Tests avec STDIN vide\*\*\*\*\*\*\\n >> fonctionnel.md
+echo -e \\n\`\`\`\\n >> fonctionnel.md
+echo -e \\n\#\#\# Tests avec STDIN vide :\\n >> fonctionnel.md
+echo -e \\n\`\`\`\\n >> fonctionnel.md
 echo -e \$ \.\/tp3 -t\\n >> fonctionnel.md
 ./tp3 -t >> fonctionnel.md
 echo -e \\n\$ \.\/tp3 -i -s -d\\n >> fonctionnel.md
 ./tp3 -i -s -d >> fonctionnel.md
 echo -e \\n\$ \.\/tp3\\n >> fonctionnel.md
 ./tp3 >> fonctionnel.md
-echo -e \\n\*\*\*\*\*\*Tests avec valgrind\*\*\*\*\*\*\\n >> fonctionnel.md
+echo -e \\n\`\`\`\\n >> fonctionnel.md
+echo -e \\n\#\#\# Tests avec valgrind\\n >> fonctionnel.md
+echo -e \\n\`\`\`\\n >> fonctionnel.md
 echo -e \\n\$ cat \.github\/workflows\/Fichier-tests-f\.txt \| valgrind \.\/tp3 -i -s -d\\n >> fonctionnel.md
 cat .github/workflows/Fichier-tests-f.txt | valgrind ./tp3 -i -s -d &>> fonctionnel.md
 echo -e \\n\$ \.\/tp3 -t -i -dd -sfg -sffgg\\n >> fonctionnel.md
